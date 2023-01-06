60774a86c0fb9e21d22b60e9cabc678155311814
Nasrullah <89463001+nasdevs@users.noreply.github.com>
2023-01-06 20:34:53 +0800

    Initial commit

---
+++ A_Sound-Chimera.cpp
@@ -0,0 +1,15 @@
+/*
+contest : https://tlx.toki.id/contests/troc-29
+problem : https://tlx.toki.id/problems/troc-29/A
+created : 29.12.2022
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    cout << (a == b) << '\n';
+}
